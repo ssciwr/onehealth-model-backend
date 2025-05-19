0b00a782e8ffb4f057118dd029321a89195c34a5
Edwin Carreño <ecarrenolozano@gmail.com>
2025-05-19 16:11:52 +0200

    chores: add logging response when the dataset has been successfully loaded as xarray array.

--- src/mypackage/read_geodata.py
+++ src/mypackage/read_geodata.py
@@ -9,6 +9,9 @@ import xarray as xr
 from typing import Optional
 
 
+logging.basicConfig(level=logging.INFO)
+
+
 def read_netcdf4_format(path_file: str) -> Optional[xr.Dataset]:
     """Read NETCDF4 files as xarray datasets.
 
@@ -31,7 +34,11 @@ def read_netcdf4_format(path_file: str) -> Optional[xr.Dataset]:
         return None
 
     try:
-        return xr.open_dataset(path, engine="netcdf4")
+        xarray_ds = xr.open_dataset(path, engine="netcdf4")
+        logging.info(
+            f"Function read_netcdf4_format({path_file}) has been successfully loaded."
+        )
+        return xarray_ds
     except Exception as e:
         logging.error(f"Error opening NetCDF4 file {path}: {e}")
         return None
