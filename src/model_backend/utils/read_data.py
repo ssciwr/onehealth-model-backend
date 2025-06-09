c53b7efcf14aa140196afab9aacf3f09a09e7f63
Harald  Mack <harald.mack@protonmail.com>
2025-06-09 22:31:21 +0200

    work on map clipping

--- src/model_backend/utils/read_data.py
+++ src/model_backend/utils/read_data.py
@@ -7,7 +7,7 @@ import logging
 from pathlib import Path
 import xarray as xr
 from typing import Optional
-
+import geopandas as gpd
 
 logging.basicConfig(level=logging.INFO)
 
@@ -41,4 +41,27 @@ def read_netcdf4_format(path_file: str) -> Optional[xr.Dataset]:
         return xarray_ds
     except Exception as e:
         logging.error(f"Error opening NetCDF4 file {path}: {e}")
+        return None
+
+
+def get_eurostat_geospatial(
+    nuts_level=3,
+    year=2024,
+    resolution="10M",
+    base_url="https://gisco-services.ec.europa.eu/distribution/v2/nuts",
+    url=lambda base_url, resolution, year, nuts_level: f"{base_url}/geojson/NUTS_RG_{resolution}_{year}_4326_LEVL_{nuts_level}.geojson",
+):
+    # TODO: check again that this works as intended. eurostat has no good python package anymore.
+    # alternatively use r2py and the r package instead
+
+    # Available formats: geojson, shp, topojson
+    url_str = url(
+        nuts_level=nuts_level, year=year, resolution=resolution, base_url=base_url
+    )
+
+    try:
+        nuts_data = gpd.read_file(url_str)
+        return nuts_data
+    except Exception as e:
+        print(f"Failed to download from {url}: {e}")
         return None
