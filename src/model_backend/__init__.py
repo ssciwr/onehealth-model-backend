252abc94e85d327a69d94ae1f8ee88689248693b
Harald Mack <harald.mack@protonmail.com>
2025-08-04 14:11:07 +0200

    add dynamic versioning based on tags, continuous deployment action.

--- src/model_backend/__init__.py
+++ src/model_backend/__init__.py
@@ -2,5 +2,14 @@ from . import computation_graph
 from . import utils
 from . import Jmodel
 
+# Import the version from the package metadata or provide a fallback
+# if that is not possible
+try:
+    from importlib.metadata import version
+
+    __version__ = version("oneHealthModels")
+except ImportError:
+    __version__ = "unknown"
+
 # Optional: Define what gets imported with "from model_backend import *"
-__all__ = ["Jmodel", "utils", "computation_graph"]
+__all__ = ["Jmodel", "utils", "computation_graph", "__version__"]
