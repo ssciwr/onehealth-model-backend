5c9b91282fdcc973f2be216fd50aae6eb48a3a35
Edwin Carreño <ecarrenolozano@gmail.com>
2025-08-26 14:19:33 +0200

    chores: create a container (dataclass) to store all the xarray datasets

---
+++ src/heiplanet_models/Pmodel/config.py
@@ -0,0 +1,32 @@
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+# ---- Paths to files
+PATH_DATASETS_SANDBOX = {
+    "TEMPERATURE": Path("data/in/Pratik_datalake/temperature_dummy.nc"),
+    "RAINFALL": Path("data/in/Pratik_datalake/pr_dummy.nc"),
+    "HUMAN_POPULATION": Path("data/in/Pratik_datalake/dense_dummy.nc"),
+}
+
+PATH_DATASETS_PRODUCTION = {
+    "TEMPERATURE": Path(
+        "data/in/Pratik_datalake/ERA5land_global_t2m_daily_0.5_2024.nc"
+    ),
+    "RAINFALL": Path("data/in/Pratik_datalake/ERA5land_global_tp_daily_0.5_2024.nc"),
+    "HUMAN_POPULATION": Path("data/in/Pratik_datalake/pop_dens_2024_global_0.5.nc"),
+}
+
+# ---- Configuration load data
+CHUNKING_SCHEME = {
+    "longitude": 90,
+    "latitude": 45,
+    "time": 10,
+}
+COORDINATES_ORDER = ("longitude", "latitude", "time")
+
+# ---- ODE
+MODEL_VARIABLES = ["eggs", "ed", "juv", "imm", "adults"]
+TIME_STEP = 10
