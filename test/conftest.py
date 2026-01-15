1dcb13867123d3358af719319d5d179cd73c0f88
Harald Mack <harald.mack@protonmail.com>
2026-01-15 10:59:11 +0100

    remove invalid resolution option from test data fixture: not responsibility of model backend

--- test/conftest.py
+++ test/conftest.py
@@ -4,15 +4,9 @@ import xarray as xr
 from pathlib import Path
 
 
-def make_rioxarray_testdata(
-    path: Path, valid: bool = True, resolution: int = 10
-) -> xr.Dataset:
-    if valid:
-        lon = np.linspace(-180, 180, resolution)
-        lat = np.linspace(-90, 90, resolution)
-    else:
-        lon = np.linspace(-120, 120, resolution)
-        lat = np.linspace(-70, 76, resolution)
+def make_testdata_impl(path: Path, resolution: int = 10) -> xr.Dataset:
+    lon = np.linspace(-180, 180, resolution)
+    lat = np.linspace(-90, 90, resolution)
 
     # create test data but not random so tests are reproducible
     t_min = -25.0
@@ -49,12 +43,7 @@ def make_rioxarray_testdata(
 
 @pytest.fixture
 def make_test_data(tmp_path) -> xr.Dataset:
-    return make_rioxarray_testdata(tmp_path, valid=True, resolution=50)
-
-
-@pytest.fixture
-def make_invalid_test_data(tmp_path) -> xr.Dataset:
-    return make_rioxarray_testdata(tmp_path, valid=False, resolution=50)
+    return make_testdata_impl(tmp_path, resolution=50)
 
 
 # for execution pipeline
